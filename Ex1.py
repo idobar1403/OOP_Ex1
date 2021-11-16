7b98d979d1a3130505493076a11b34d1e3bc9000
Yehudit-Brickner <nech.diti@yahoo.com>
2021-11-16 17:44:11 +0200

    changed signes, and added func to find where elev is when given a time

--- Ex1.py
+++ Ex1.py
@@ -1,8 +1,5 @@
 import json
 import sys
-import subprocess
-
-import numpy as np
 import pandas as pd
 import math
 
@@ -18,12 +15,26 @@ class Elevator:
         self.stopTime=Elevator['_stopTime']
         self.callList=[]
 
-    def _str_(self):
-        s1="elevator="
-        s2=self.id
-        s3=self.speed
-        s=s1 , s2,s3
-        return s
+    def whereAtTime(self, time):
+        count = 0
+        if (len(self.callList) > 0):
+            for i in range(1, len(self.callList)):
+                if (time >= self.callList[i - 1].runTime and time <= self.callList[i].runTime):
+                    if (self.callList[i - 1].floor < self.callList[i].floor):
+                        timeForFloor = abs(self.callList[i - 1].floor - self.callList[i].floor) / (
+                                    self.callList[i].runTime - self.callList[i - 1].runTime)
+                        curr = math.ceil((time - self.callList[i - 1].runTime) * timeForFloor) + self.callList[
+                            i - 1].floor
+                        return int(curr)
+                    else:
+                        timeForFloor = abs(self.callList[i - 1].floor - self.callList[i].floor) / (
+                                    self.callList[i].runTime - self.callList[i - 1].runTime)
+                        curr = self.callList[i - 1].floor - math.ceil(
+                            (time - self.callList[i - 1].runTime) * timeForFloor)
+                        return int(curr)
+                count += 1
+        else:
+            return 0
 
 
 class Building:
@@ -31,19 +42,8 @@ class Building:
         self.minFloor=building['_minFloor']
         self.maxFloor=building['_maxFloor']
         self.ElevList=[]
-    def _str_(self):
-        s1="minFloor="
-        s2=self.minFloor
-        s3="maxFloor="
-        s4=self.maxFloor
-        s5="numOfElev="
-        s6=self.numOfElev
-        s=s1 , s2 , s3 , s4 , s5 , s6
-        return s
 
-
-
-class Pairs:
+class Triplet:
     def __init__(self, floor, minTime, runTime):
         self.floor=floor
         self.minTime=minTime
@@ -54,18 +54,21 @@ class Pairs:
         speedTime=dist/x.speed
         time=constTime+speedTime+self.runTime
         return time
+    def changeTime(self,p2,x):
+        sum=0
+        #dist=abs(self.floor-p2.floor)
+        #constTime=x.closeTime+x.openTime+x.startTime+x.stopTime
+        #speedTime=dist/x.speed
+        #time=constTime+speedTime+self.runTime
+        time=self.findTime(p2,x)
+        if (p2.runTime<time):
+            sum+=time-p2.runTime
+            p2.runTime=time
+        return sum
     def copy(self):
         return type(self)(self.floor,self.minTime,self.runTime)
-def changeTime(p1,p2,x):
-    sum=0
-    dist=abs(p1.floor-p2.floor)
-    constTime=x.closeTime+x.openTime+x.startTime+x.stopTime
-    speedTime=dist/x.speed
-    time=constTime+speedTime+p1.runTime
-    if (p2.runTime<time):
-        sum+=time-p2.runTime
-        p2.runTime=time
-    return sum
+
+
 
 def findElev(ElevList,src,dest):
     if(len(ElevList)==1):
@@ -86,7 +89,7 @@ def findElev(ElevList,src,dest):
         for y in range(1,len(temp)):
             time= temp[y-1].findTime(src1,x)
             if (time>src1.runTime):
-                if((src1.floor>temp[y-1].floor and src1.floor<temp[y].floor)or(src1.floor>temp[y].floor and src1.floor<temp[y-1].floor)):
+                if((src1.floor>=temp[y-1].floor and src1.floor<=temp[y].floor)or(src1.floor>=temp[y].floor and src1.floor<=temp[y-1].floor)):
                     temp.insert(y,src1)
                     i=y
                     break
@@ -95,7 +98,7 @@ def findElev(ElevList,src,dest):
             for z in range(i+1,len(temp)):
                 time=temp[z-1].findTime(dest1,x)
                 if(time>dest1.runTime):
-                    if((dest1.floor>temp[z-1].floor and dest1.floor<temp[z].floor)or(dest1.floor>temp[z].floor and dest1.floor<temp[z-1].floor)):
+                    if((dest1.floor>=temp[z-1].floor and dest1.floor<=temp[z].floor)or(dest1.floor>=temp[z].floor and dest1.floor<=temp[z-1].floor)):
                         temp.insert(z, dest1)
                         j = z
                         break
@@ -109,7 +112,7 @@ def findElev(ElevList,src,dest):
             temp.insert(dest1)
         temp[0].runTime=temp[0].minTime+x.closeTime+x.startTime
         for y in range(1,len(temp)):
-            sum=sum+changeTime(temp[y-1],temp[y],x)
+            sum=sum+temp[y-1].changeTime(temp[y],x)
         if(sum<min):
             index=x.id
             place=count
@@ -119,9 +122,7 @@ def findElev(ElevList,src,dest):
     ElevList[place].callList = temp1
     return index
 
-
-
-def readfiles(s1, s2,s3):
+def readfiles(s1, s2):
     try:
         with open(s1) as f:
             building=json.load(f)
@@ -130,29 +131,41 @@ def readfiles(s1, s2,s3):
     mybuilding = Building(building)
     count=0
     for e in building['_elevators']:
-       count_e=Elevator(e)
-       mybuilding.ElevList.append(count_e)
-       count=count+1
+        count_e=Elevator(e)
+        mybuilding.ElevList.append(count_e)
+        count=count+1
     try:
-         calls = pd.read_csv(s2,header=None)
-         output = pd.read_csv(s3,header=None)
+        calls = pd.read_csv(s2,header=None)
+        output = pd.read_csv(s2,header=None)
     except:
         print("Not csv file")
 
-   # output = calls.copy()
+    # output = calls.copy()
     for x in calls.itertuples():
-        source=Pairs(x[3],x[2],x[2])
-        dest= Pairs(x[4], 0.0, 0.0)
+        source=Triplet(x[3],x[2],x[2])
+        dest= Triplet(x[4], 0.0, 0.0)
         output.loc[x[0],5]=findElev(mybuilding.ElevList, source,dest)
-        calls.loc[x[0],5]=output.loc[x[0],5]
-    output.to_csv(r'/Users/idobar/Bsc/Year 2/OOP/Ex1/OOP_Ex1/out.csv', index=False, header=False)
-    #return output
-if __name__ == '__main__':
+        #calls.loc[x[0],5]=output.loc[x[0],5]
+    output.to_csv(r'_'+s1+'_'+s2+'.csv', header=False, index=False)
+    # return output
 
-    # with open(sys.argv[0], 'r') as f:
-    #     build = f.read()
+
+
+if __name__ == '__main__':
+   # readfiles("B1.json",
+    #       "Calls_a.csv")
+
+    list1=["B1.json","B2.json","B3.json","b4.json","B5.json"]
+    list2=["calls_a.csv", "calls_b.csv","calls_c.csv", "calls_d.csv"]
+
+    for x in range (0, len(list1)):
+        for y in range(0,len(list2)):
+            readfiles(list1[x],list2[y])
+
+    #   with open(sys.argv[0], 'r') as f:
+    #      build = f.read()
     # with open(sys.argv[1], 'r') as f:
     #     calls = f.read()
     # with open(sys.argv[2], 'r') as f:
     #     out = f.read()
-    readfiles(sys.argv[1],sys.argv[2],sys.argv[3])
+    # readfiles(sys.argv[1],sys.argv[2],sys.argv[3]
